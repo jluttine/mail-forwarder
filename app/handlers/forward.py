7bbf6666cbb77e701f50e6b65294ebe396a4c8f2
Jaakko Luttinen <jaakko.luttinen@iki.fi>
2014-10-16 00:22:23 -0400

    Allow each alias to have a different mapping

--- app/handlers/forward.py
+++ app/handlers/forward.py
@@ -1,13 +1,19 @@
 from lamson.routing import route, stateless
-from config.settings import relay, forward_address, receive_addresses
+from config.settings import relay, forward_mapping
 
 
 @route("(address)@(host)", address=".+")
 @stateless
 def FORWARD(message, address=None, host=None):
     target = address.lower() + "@" + host.lower()
-    if target in receive_addresses:
-        relay.deliver(message, To=[forward_address])
+    to = []
+    for i in range(len(forward_mapping)):
+        alias = forward_mapping[i][0].lower()
+        real = forward_mapping[i][1]
+        if target == alias:
+            to.append(real)
+    if len(to) > 0:
+        relay.deliver(message, To=to)
     else:
         raise Exception("Not forwarding email: %s not an accepted email address" % target)
 
